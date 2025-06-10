91d462f773106fd1bff0640f176a1454e5d439f8
nelson canales <nelson.canales@agibiz.cl>
2025-06-10 00:00:24 +0000

    subi todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OperacionesController } from './operaciones/operaciones.controller';
+import { OperacionesService } from './operaciones/operaciones.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, OperacionesController],
+  providers: [AppService, OperacionesService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola mundo!';
+    return 'Hello World!';
   
   }
   getHallo():string{
